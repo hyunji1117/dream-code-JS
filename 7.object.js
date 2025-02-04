44179b8bd6f3129b66f344d488abc0a19fe2ebab
Evelyn.Kim <eve0204eve@gmail.com>
2025-02-05 07:09:17 +0900

    about object

---
+++ 7.object.js
@@ -0,0 +1,33 @@
+let name = 'apple';
+let color = 'red';
+let display = '🍎';
+let orangeName = 'orange';
+
+/**
+ * 객체: 
+ * 연관된 데이터를 한 그룹으로 패키지화 시킬 수 있는 것
+*/
+
+let apple = {
+  name: 'apple',
+  color: 'red',
+  display: '🍎',
+};
+
+console.log(apple);
+console.log(apple.name);
+console.log(apple.color);
+console.log(apple.display);
+
+
+let orange = {
+  name: 'orange',
+  color: 'orange',
+  display: '🍊',
+};
+
+console.log(orange);
+console.log(orange.name);
+console.log(orange.color);
+console.log(orange.display);
+
